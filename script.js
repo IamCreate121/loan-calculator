4e0c512dedc91593bbfe45e640b8d549f4428bfa
Sanmi Israel <86537780+IamCreate121@users.noreply.github.com>
2026-03-13 11:24:03 +0100

    Add script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //--Welcome Message--/
-document.getElementById('welcome-message').innerText = "I will be showing step by step how I build this tool"
+document.getElementById('welcome-message').innerText = "I will be showing step by step how I build this tool";
