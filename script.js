857d5e4fb52eb914d1a4c8da2dc19c4d7335b831
Sanmi Israel <86537780+IamCreate121@users.noreply.github.com>
2026-03-17 16:11:23 +0100

    Add script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //--Welcome Message--/
 document.getElementById('welcome-message').innerText = "I will be showing step by step how I build this tool";
+//
+const getBest = 'Single string'
+ const getBetter = `Hello, ${getBest}`
+console.log(getBetter);
+
+
+
+
